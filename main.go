c4ac718cf22495b20cc7786151f45a701045ada5
HowDoIprintHelloWorld <howdoiprintgit.ql4lv@simplelogin.com>
2024-07-22 13:27:32 +0200

    Connected prompt and parser in main.go

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	parsing "hyperterm/src/parsing"
 	termio "hyperterm/src/termio"
 )
 
 func main() {
 	prompt := termio.Make_prompt("%red[$t:H$:$t:M$:$t:S$]  ")
+	parser := parsing.NewParser()
 	for {
 		line, _ := prompt.Get_line()
-		fmt.Println(line)
+		commands, _ := parser.Parse(line)
+		for _, command := range commands {
+			fmt.Print(command, " | ")
+		}
+		fmt.Println()
 	}
 }
